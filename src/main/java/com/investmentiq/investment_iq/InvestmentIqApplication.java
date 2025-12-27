9f3a5c17bc78b95fc787d51757906fe72bf99827
AbirPaulDev <rickabir9@gmail.com>
2025-12-27 12:01:40 +0530

    Project Initialization

---
+++ src/main/java/com/investmentiq/investment_iq/InvestmentIqApplication.java
@@ -0,0 +1,13 @@
+package com.investmentiq.investment_iq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentIqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentIqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/investmentiq/investment_iq/InvestmentIqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.investmentiq.investment_iq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentIqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
